5d55207f4cd49918a288c55b8bb4b2652b6a32d0
Qingyang Duan <duanqy99@163.com>
2022-11-06 12:22:29 +0800

    Delete BS-ForMXNet-problems directory

--- BS-ForMXNet-problems/init.sh
+++
@@ -1,9 +0,0 @@
-
-sudo apt install ssh
-sudo systemctl enable ssh
-
-sudo apt update
-sudo apt install net-tools
-sudo apt install ethtool
-
-
--- BS-ForMXNet-problems/install_conda.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-export MY_PATH="/usrs/duanqing"
-
-cd $MY_PATH
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
